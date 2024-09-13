f4e97bbdc6935bf3c184f21b6199b474c736f6e9
DevExpressExampleBot <fakeEmail>
2024-09-13 17:23:51 +0400

    Source auto update [skip ci]

--- VB/Default.aspx
+++ VB/Default.aspx
@@ -1,15 +1,15 @@
-<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="GridRuntime._Default" %>
+<%@ Page Language="VB" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="GridRuntime._Default" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
-	<title>Untitled Page</title>
+    <title>Untitled Page</title>
 </head>
 <body>
-	<form id="form1" runat="server">
-	<div>
-	</div>
-	</form>
+    <form id="form1" runat="server">
+    <div>
+    </div>
+    </form>
 </body>
 </html>
--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -1,10 +1,5 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Data
-Imports System.Configuration
-Imports System.Collections
-Imports System.Web
-Imports System.Web.Security
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports System.Web.UI.WebControls.WebParts
@@ -12,26 +7,26 @@ Imports System.Web.UI.HtmlControls
 Imports DevExpress.Web
 
 Namespace GridRuntime
-	Partial Public Class _Default
-		Inherits System.Web.UI.Page
-		Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
-			Dim grid As New ASPxGridView()
-			grid.ID = "grid"
-			grid.KeyFieldName = "ID"
-			grid.DataSource = GetData()
 
-			Me.form1.Controls.Add(grid)
+    Public Partial Class _Default
+        Inherits Page
 
-			grid.DataBind()
-		End Sub
+        Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+            Dim grid As ASPxGridView = New ASPxGridView()
+            grid.ID = "grid"
+            grid.KeyFieldName = "ID"
+            grid.DataSource = GetData()
+            form1.Controls.Add(grid)
+            grid.DataBind()
+        End Sub
 
-		Public Function GetData() As DataTable
-			Dim Table As New DataTable()
-			Table.Columns.Add("ID", GetType(Integer))
-			Table.Columns.Add("ItemName", GetType(String))
-			Table.Rows.Add(1, "A")
-			Table.Rows.Add(2, "B")
-			Return Table
-		End Function
-	End Class
+        Public Function GetData() As DataTable
+            Dim Table As DataTable = New DataTable()
+            Table.Columns.Add("ID", GetType(Integer))
+            Table.Columns.Add("ItemName", GetType(String))
+            Table.Rows.Add(1, "A")
+            Table.Rows.Add(2, "B")
+            Return Table
+        End Function
+    End Class
 End Namespace
